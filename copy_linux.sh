bb7e22414dd039a4fb35869720f86f56d4126cb2
Tony Wang <hellotony@vista.aero>
2025-11-24 12:52:29 +0800

    *Updated the copy script for linux.

--- copy_linux.sh
+++ copy_linux.sh
@@ -10,6 +10,9 @@ else
 fi
 
 if [ -d "$BASEDIR/build/.libs" ]; then
+  if [ ! -d "$BASEDIR/lib/$SUBDIR/" ]; then
+    mkdir -p "$BASEDIR/lib/$SUBDIR/"
+  fi
   cp -f "$BASEDIR/build/.libs/libSDL2_mixer.a" "$BASEDIR/lib/$SUBDIR/"
   cp -f "$BASEDIR/build/.libs/libSDL2_mixer.la" "$BASEDIR/lib/$SUBDIR/"
   cp -f "$BASEDIR/build/.libs/libSDL2_mixer-2.0.so.0.2.2" "$BASEDIR/lib/$SUBDIR/"
